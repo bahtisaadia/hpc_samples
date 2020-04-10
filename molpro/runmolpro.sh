75d9034fe9c6d542970dac82520f0ae146eb0024
rahimbouchra <rahim.bouchra@gmail.com>
2020-04-10 10:39:01 +0100

    Create runmolpro.sh

---
+++ molpro/runmolpro.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export MOLPROROOT=/opt/molpro2010/molpros_2010_1_Linux_x86_64_i8/
+export PATH=$PATH:$MOLPROROOT/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MOLPROROOT/lib
+export WFUPATH=/data/$USER/tmp/wfu
+mkdir -p $WFUPATH
+molpro -W $WFUPATH $1
