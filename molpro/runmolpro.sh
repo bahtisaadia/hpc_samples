aa553930ea52ef5ee47d72c2f7689acd8b640752
rahimbouchra <rahim.bouchra@gmail.com>
2020-04-13 12:33:39 +0100

    Update runmolpro.sh

--- molpro/runmolpro.sh
+++ molpro/runmolpro.sh
@@ -2,6 +2,6 @@
 export MOLPROROOT=/opt/molpro2010/molpros_2010_1_Linux_x86_64_i8/
 export PATH=$PATH:$MOLPROROOT/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MOLPROROOT/lib
-export WFUPATH=/data/$USER/tmp/wfu
+export WFUPATH=/mnt/workdir/wfu
 mkdir -p $WFUPATH
 molpro -W $WFUPATH $1
